47f2ffdaea8885b9cff347d25e2d6ab3f0499078
Logali-Group <justific.ar.1@gmail.com>
2022-03-17 18:30:41 +0000

    Prueba segundo commit

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -8,8 +8,24 @@ sap.ui.define([
         "use strict";
 
         return Controller.extend("logaligroup.invoices.controller.View1", {
+
             onInit: function () {
+                const oJSONModel = new sap.ui.model.json.JSONModel();
+                const oView = this.getView();
+                oJSONModel.loadData("./model/SelectionScreenMenu.json");
+                oView.setModel(oJSONModel, "selectionScreen");
+            },
+
+            onFilter: function (oEvent) {
+
+            },
+
+            onClearFilter: function (oEvent) {
+                const oModelSelScreen = this.getView().getModel("selectionScreen");
+                oModelSelScreen.setProperty("/CountryKey", "");
+                oModelSelScreen.setProperty("/ShipName", "");
 
             }
+
         });
     });
